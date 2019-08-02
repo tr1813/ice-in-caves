b94c348973ce6b54383ca73d96031a1e6bdc716c
tr1813 <tang.mich.root@gmail.com>
2019-08-02 11:46:21 +0200

    Update GNIP_reader.py
    adding comments
    and customisation of median line

--- GNIP_reader.py
+++ GNIP_reader.py
@@ -22,17 +22,28 @@ def GNIP_weighted_avg(dataframe,series,weights):
 	return year_avg
 
 def GNIP_quarterly_boxplot(dataframe,filename='sample_boxplot.pdf'):
-	fig,(ax_O18,ax_H2) = plt.subplots(1,2,figsize=(10,5))
+	
+	#create a figure with two subplots. Size 8 inches by 5
+	fig,(ax_H2,ax_O18) = plt.subplots(1,2,figsize=(8,5))
 
+	#define the quarters. 
 	quarters = ["Winter","Spring","Summer","Autumn"]
 
 	for ax,series,labels in zip((ax_H2,ax_O18),("H2","O18"),("$\delta^{2}$H","$\delta^{18}$O")):
-		dataframe.boxplot(column=[series], by=dataframe.index.quarter, ax=ax, showfliers=True,grid=False)
+		bp = dataframe.boxplot(column=[series], 
+			by=dataframe.index.quarter, 
+			ax=ax, 
+			showfliers=True,
+			grid=False,return_type = 'dict')
 
-		ax.set_ylabel(labels+" [‰]")
+		ax.set_ylabel(labels+" [‰] (VSMOW)")
 		ax.set_xlabel("")
 		ax.set_xticklabels(quarters)
 		ax.tick_params(direction='in',top=True)
+		[[item.set_color('firebrick') for item in bp[key]['medians']] for key in bp.keys()]
+		[[item.set_color('black') for item in bp[key]['boxes']] for key in bp.keys()]
+		[[item.set_color('black') for item in bp[key]['whiskers']] for key in bp.keys()]
+
 
 	plt.suptitle("")
 	plt.tight_layout()
